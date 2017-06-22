01a33e748a2354fb39bba6e3a868572f9110a16c
Nicolò Mazzoli <nico.mazzoli@hotmail.it>
2017-06-22 23:16:26 +0200

    Gestione modifiche alle posizioni dei Beacon

--- app/src/main/java/com/example/user/iot/controller/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/user/iot/controller/MyFirebaseMessagingService.java
@@ -4,8 +4,18 @@ import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.user.iot.R;
+import com.example.user.iot.model.BeaconDataSource;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONArray;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
@@ -52,14 +62,39 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 //        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
 //        notificationManager.notify(0 /*ID of notification*/, notifiBuilder.build());
 
-        Intent resIntent = new Intent(title);
-        if(!body.equals("null")){
-            resIntent.putExtra("dove", body);
-        }
-        LocalBroadcastManager.getInstance(this).sendBroadcast(resIntent);
+        if(title.equals("Beacon Cambiati")){
+            RequestQueue mRequestQueue= Volley.newRequestQueue(this);
+            JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET,getResources().getString(R.string.getBeacon), null, postListenerJsonArray, errorListener);
+            mRequestQueue.add(request);
+        } else{
+            Intent resIntent = new Intent(title);
+            if(!body.equals("null")){
+                resIntent.putExtra("dove", body);
+            }
+            LocalBroadcastManager.getInstance(this).sendBroadcast(resIntent);
 
-        Intent resIntent2 = new Intent("alert");
-        LocalBroadcastManager.getInstance(this).sendBroadcast(resIntent2);
+            Intent resIntent2 = new Intent("alert");
+            LocalBroadcastManager.getInstance(this).sendBroadcast(resIntent2);
+        }
+
     }
+
+    private Response.Listener<JSONArray> postListenerJsonArray= new Response.Listener<JSONArray>(){
+        @Override
+        public void onResponse(JSONArray response) {
+
+            BeaconDataSource datasource =  new BeaconDataSource(MainActivity.context);
+            datasource.open();
+            datasource.updateBeacon(response);
+        }
+    };
+
+    private Response.ErrorListener errorListener=new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError err)
+        {
+            Log.d(MainActivity.context.getResources().getString(R.string.serverError), "Errore di rete. Non è stato possibile accedere al server");
+        }
+    };
 
 }
